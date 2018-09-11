f3ba09a3b56e0f0b8f479cd29782ea26b997b870
raquelmv <raquel.mvillarreal@gmail.com>
2018-09-10 21:36:08 -0500

    unsolved doughnut shart

--- piggy-bank/src/containers/home.js
+++ piggy-bank/src/containers/home.js
@@ -1,16 +1,42 @@
 import React, {Component} from 'react';
-import Main from '../components/home/main'
+import Main from '../components/home/Main';
+import Chart from '../components/home/Chart';
+
 
 class Home extends Component {
-    state = {
-        name: "jorge",
-        age: 25
-    };
+    constructor() {
+        super();
+        this.state = {
+          chartData: {
+            
+          }
+        }
+      }
+      componentWillMount(){
+        this.getChartData()
+      }
+    
+      getChartData() {
+        this.setState({
+          chartData: {
+            //axios call
+            labels: ['Fijos', 'Hormiga', 'treats', 'Social', 'Ahorro'],
+            datasets: [{
+              label: 'Gastos',
+              data: [40,19,11,22,18],
+              backgroundColor: ['#E10024','#EEA845','#5CD9EA','#F69BF1','#6DF5BE',
+              ]
+            }
+            ]
+          }
+        })
+      }
 
     render(){
         return(
             <div>
                 <Main name={this.state.ninja}/>
+                <Chart chartData={this.state.chartData} legendPosition="bottom" />
             </div>
         )
     }
